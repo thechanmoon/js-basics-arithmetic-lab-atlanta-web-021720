6197e344825e0578bfda225d1db69ce15609706f
thechanmoon <thechanmoon@gmail.com>
2020-03-29 15:16:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code here
+
+// const var1 = 2, var2 = 31;
+// const multiply = var1*var1;
+
+
+const var1 = 2
+const var2 = 31
+const multiply = var1 * var2
+const random = Math.floor(Math.random() * 10 ) + 1
+const mod = 16 % 6
+const max = Math.max(11, 20)
+
+
+// function random()
+// {
+//     return (Math.random()*100);
+// } 
+
+// function mod(arg)
+// {
+//     return arg % 4
+// }
+
+// function max() 
+// {
+
+// }
